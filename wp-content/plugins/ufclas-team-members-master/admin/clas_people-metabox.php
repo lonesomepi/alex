f85f1f442d573889cc117b653067d7087dce1e20
ahuntercatalano <alex.catalano2@gmail.com>
2020-02-05 15:48:41 -0500

    update this

--- wp-content/plugins/ufclas-team-members-master/admin/clas_people-metabox.php
+++ wp-content/plugins/ufclas-team-members-master/admin/clas_people-metabox.php
@@ -88,7 +88,7 @@ function member_info_display_meta_box( $post ) {
 	?>
 
 	<textarea name="name" rows="8" cols="80" placeholder="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></textarea>
-
+	<p>add a graph</p>
 
 <?php
 
